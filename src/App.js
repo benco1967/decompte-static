d8c978168ed48d1af17d891dc8a4475443948faa
Benco1967 <benco1967@gmail.com>
2019-01-27 17:52:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,159 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import  axios  from 'axios';
 import './App.css';
+
+
+class Index extends Component {
+
+  state= {};
+  inputs = {};
+  send = async () => {
+    const code = this.inputs.code.value;
+    const pseudo = this.inputs.pseudo.value;
+    if(code.match(/^[a-z0-9]{4}$/) && pseudo) {
+      console.log(`send ${pseudo} ${code}`);
+      this.inputs.button.disabled = true;
+
+      try {
+        const result = await axios.post("https://ipw21gnfgd.execute-api.eu-west-1.amazonaws.com/dev/score", {pseudo, code});
+        console.log(JSON.stringify(result));
+        this.setState({message: "Vos points ont été enregistrés"});
+      } catch (e) {
+        console.log(JSON.stringify(e));
+        this.setState({message: e.response.data.message});
+      }
+      this.inputs.button.disabled = false;
+    }
+    else {
+      this.setState({message: "Code et/ou Pseudo invalide"});
+    }
+    setTimeout(() => this.setState({message: ''}), 5000);
+  };
+  render() {
+    return (<div>
+      <h1>Ajout de points</h1>
+      <div><label>pseudo</label> <input name="pseudo" placeholder="votre pseudo" required ref={input => this.inputs.pseudo = input} type="text"/></div>
+      <div><label>code</label> <input name="code" placeholder="code à 4 caractères" required ref={input => this.inputs.code = input} type="text"/></div>
+      <button ref={input => this.inputs.button = input} onClick={this.send}>Envoyer</button>
+      <div>{this.state.message}</div>
+    </div>);
+  }
+}
+
+class Code extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: props.match.params.code !== 'blastpower' ? "vous n'êtes pas autorisé": "",
+      authorized: props.match.params.code === 'blastpower'
+    };
+
+  }
+  inputs = {};
+  send = async () => {
+    const label = this.inputs.label.value;
+    const points = this.inputs.points.value;
+    const nbDays = this.inputs.nbDays.value;
+    const nbPlayers = this.inputs.nbPlayers.value;
+    if(label && points > 0 && nbDays > 0 && nbPlayers > 0) {
+      console.log(`send ${label} ${points} ${nbDays} ${nbPlayers}`);
+      this.inputs.button.disabled = true;
+
+      try {
+        const result = await axios.post("https://ipw21gnfgd.execute-api.eu-west-1.amazonaws.com/dev/code", {label, points, nbPlayers, nbDays});
+        console.log(JSON.stringify(result));
+
+        this.setState({code: result.data.code});
+      } catch (e) {
+        console.log(JSON.stringify(e));
+        this.setState({message: e.response.data.message});
+      }
+      this.inputs.button.disabled = false;
+    }
+    else {
+      this.setState({message: "Remplissez tous les champs"});
+    }
+    setTimeout(() => this.setState({message: ''}), 5000);
+  };
+  render({match} = this.props) {
+    return (<div>
+      <h1>Création de code</h1>
+      { this.state.authorized && (
+        <div>
+          <div><label>nom activité</label> <input name="label" placeholder="nom de l'activité" required ref={input => this.inputs.label = input} type="text"/></div>
+          <div><label>points</label> <input name="points" required ref={input => this.inputs.points = input} type="number" min={1} /></div>
+          <div><label>nombre de participants</label> <input name="nbPlayers" required ref={input => this.inputs.nbPlayers = input} type="number" min={1}/></div>
+          <div><label>nombre de jours actifs</label> <input name="nbDays" required ref={input => this.inputs.nbDays = input} type="number" min={1}/></div>
+          <button ref={input => this.inputs.button = input} onClick={this.send}>Envoyer</button>
+        </div>
+      )}
+      <div>{this.state.message}</div>
+      {this.state.code && (<div>
+        Le nouveau code est <strong>{this.state.code}</strong> (mémorisez le)
+      </div>)}
+    </div>);
+  }
+}
+
+class User extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: props.match.params.code !== 'blastpower' ? "vous n'êtes pas autorisé": "",
+      authorized: props.match.params.code === 'blastpower'
+    };
+
+  }
+  inputs = {};
+  send = async () => {
+    let pseudo = this.inputs.pseudo.value;
+    if(pseudo) {
+      pseudo = pseudo.toLocaleLowerCase();
+      console.log(`send ${pseudo}`);
+      this.inputs.button.disabled = true;
+
+      try {
+        const result = await axios.post("https://ipw21gnfgd.execute-api.eu-west-1.amazonaws.com/dev/user", {pseudo});
+        console.log(JSON.stringify(result));
+
+        this.setState({message: `le pseudo ${result.data.pseudo} a bien été enregistré`});
+      } catch (e) {
+        console.log(JSON.stringify(e));
+        this.setState({message: e.response.data.message});
+      }
+      this.inputs.button.disabled = false;
+    }
+    else {
+      this.setState({message: "Donnez un pseudo"});
+    }
+    setTimeout(() => this.setState({message: ''}), 5000);
+  };
+  render({match} = this.props) {
+    return (<div>
+      <h1>Ajout d'un pseudo</h1>
+      { this.state.authorized && (
+        <div>
+          <div><label>pseudo</label> <input name="pseudo" placeholder="nouveau pseudo" required ref={input => this.inputs.pseudo = input} type="text"/></div>
+          <button ref={input => this.inputs.button = input} onClick={this.send}>Envoyer</button>
+        </div>
+      )}
+      <div>{this.state.message}</div>
+    </div>);
+  }
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Index} />
+          <Route path="/code/:code" component={Code} />
+          <Route path="/user/:code" component={User} />
+        </div>
+      </Router>
     );
   }
 }
